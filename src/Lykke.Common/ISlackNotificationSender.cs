a15ae4da950b79b338632af1a00893d7a4e2dc19
amigin <amigin@gmail.com>
2017-02-02 14:47:52 +0200

    ISlackNotification is added

---
+++ src/Lykke.Common/ISlackNotificationSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface ISlackNotificationsSender
+    {
+        Task SendAsync(string type, string sender, string message);
+    }
+}
