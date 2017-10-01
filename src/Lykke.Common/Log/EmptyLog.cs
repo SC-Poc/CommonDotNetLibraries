018fda50e540a3ae734d8bc63dab658f5cf061bc
Sergey Nesterov <sergey.nesterov@lykke.com>
2017-10-01 19:37:25 +0300

    Added EmptyLog

---
+++ src/Lykke.Common/Log/EmptyLog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Common.Log
+{
+    public class EmptyLog : ILog
+    {
+        public static EmptyLog Instance { get; } = new EmptyLog();
+
+        public Task WriteInfoAsync(string component, string process, string context, string info, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteWarningAsync(string component, string process, string context, string info, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteErrorAsync(string component, string process, string context, Exception exeption, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteFatalErrorAsync(string component, string process, string context, Exception exeption, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteInfoAsync(string process, string context, string info, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteWarningAsync(string process, string context, string info, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task WriteFatalErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
