ff4e009369acf9c993a7dc94dc3a2279a7c23f45
Novichikhin Alexey <fieldid@gmail.com>
2017-11-14 00:22:37 +0700

    add test for double case

--- tests/Common.Tests/UtilsClass/FloatOperations.cs
+++ tests/Common.Tests/UtilsClass/FloatOperations.cs
@@ -1,11 +1,12 @@
-﻿using Xunit;
+﻿using System.Globalization;
+using Xunit;
 
 namespace Common.Tests.UtilsClass
 {
     public class FloatOperations
     {
         [Fact]
-        public void GetFixedAsString()
+        public void GetFixedAsStringDecimal()
         {
             var res1 = 0.001m.GetFixedAsString(5);
             var res2 = 15.123456m.GetFixedAsString(2);
@@ -16,6 +17,33 @@ namespace Common.Tests.UtilsClass
             Assert.Equal("15.12", res2);
             Assert.Equal("2.00", res3);
             Assert.Equal("2", res4);
+        }
+
+        [Fact]
+        public void GetFixedAsStringDouble()
+        {
+            var res1 = 0.001.GetFixedAsString(5);
+            var res2 = 15.123456.GetFixedAsString(2);
+            var res3 = 2.0.GetFixedAsString(2);
+            var res4 = 2.0.GetFixedAsString(0);
+
+            var ct = new TestDouble();
+            var res5 = ct.D.GetFixedAsString(2);
+
+            var dnan = double.NaN;
+            var res6 = dnan.GetFixedAsString(2);
+
+            Assert.Equal("0.00100", res1);
+            Assert.Equal("15.12", res2);
+            Assert.Equal("2.00", res3);
+            Assert.Equal("2", res4);
+            Assert.Equal("0.00", res5);
+            Assert.Equal("NaN", res6);
+        }
+
+        public class TestDouble
+        {
+            public double D { get; set; }
         }
     }
 }
