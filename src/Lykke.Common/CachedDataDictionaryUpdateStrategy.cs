efad5102030fcabf1c9bf814513dbf909a6c57f9
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-01-05 16:32:05 +0300

    CachedDataDictionary made synchronized

---
+++ src/Lykke.Common/CachedDataDictionaryUpdateStrategy.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+
+namespace Common
+{
+    /// <summary>
+    /// <see cref="CachedDataDictionary{TKey,TValue}"/> cache update strategies
+    /// </summary>
+    [PublicAPI]
+    public enum CachedDataDictionaryUpdateStrategy
+    {
+        /// <summary>
+        /// If cache is expired or not had been initialized yet and <see cref="CachedDataDictionary{TKey,TValue}"/>
+        /// recieves multiple concurrent calls which wants to obtain cached data,
+        /// the getData delegate of the <see cref="CachedDataDictionary{TKey,TValue}"/> constructor can be called
+        /// multiple times concurrently.
+        /// </summary>
+        AllowConcurrentUpdates,
+
+        /// <summary>
+        /// If cache is expired or not had benn initialized yet and <see cref="CachedDataDictionary{TKey,TValue}"/>
+        /// recieves multiple concurrent calls which wants to obtain cached data,
+        /// the getData delegate of the <see cref="CachedDataDictionary{TKey,TValue}"/> constructor will be called
+        /// only once. Other calls will retrieve data from the cache.
+        /// </summary>
+        UseSynchronizedUpdates
+    }
+}
