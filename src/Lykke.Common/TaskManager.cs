f6a067208add581733702d6e9217f15a08e07222
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2018-03-11 16:33:42 +0100

    Refactor TasksManager. Add a cancellation token into arguments. Validate arguments.

--- src/Lykke.Common/TaskManager.cs
+++ src/Lykke.Common/TaskManager.cs
@@ -1,59 +1,117 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace Common
 {
-    public class TasksManager<TKey, TResult>
+    /// <summary>
+    /// Helps working with a collection of tasks and perform group operations with them.
+    /// </summary>
+    /// <typeparam name="TResult">A result type</typeparam>
+    public sealed class TasksManager<TResult>
     {
+        private readonly ConcurrentDictionary<object, TaskCompletionSource<TResult>> _tasks = new ConcurrentDictionary<object, TaskCompletionSource<TResult>>();
 
-        private readonly Dictionary<TKey, TaskCompletionSource<TResult>> _tasks = new Dictionary<TKey, TaskCompletionSource<TResult>>();
-
-        public Task<TResult> Add(TKey key)
+        /// <summary>
+        /// Creates a new task and add it into the internal collection
+        /// </summary>
+        /// <param name="key">A key</param>
+        /// <param name="cancellationToken">An optional cancellation token. Can be used to set the task into the canceled state</param>
+        /// <returns>The created task</returns>
+        public Task<TResult> Add([NotNull] object key, CancellationToken cancellationToken = default(CancellationToken))
         {
-            lock (_tasks)
+            if (key == null)
             {
-                var task = new TaskCompletionSource<TResult>(key);
-                _tasks.Add(key, task);
+                throw new ArgumentNullException(nameof(key));
+            }
 
-                return task.Task;
+            var task = new TaskCompletionSource<TResult>(key);
+            if (_tasks.TryAdd(key, task) && !cancellationToken.IsCancellationRequested)
+            {
+                cancellationToken.Register(() => Cancel(key, cancellationToken));
+            }
+
+            return task.Task;
+        }
+
+        /// <summary>
+        /// Attempts to transition the  task into the <see cref="F:System.Threading.Tasks.TaskStatus.RanToCompletion"></see> state.
+        /// </summary>
+        /// <param name="key">The task key</param>
+        /// <param name="result">The <paramref name="result"/> to set into the task</param>
+        public void SetResult([NotNull] object key, TResult result)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (_tasks.TryRemove(key, out var task))
+            {
+                task.TrySetResult(result);
             }
         }
 
-        public void Compliete(TKey key, TResult result)
+        private IEnumerable<TaskCompletionSource<TResult>> GetAndDeleteAll()
         {
-            lock (_tasks)
-            {
-                var theTask = _tasks[key];
-                _tasks.Remove(key);
-                theTask.SetResult(result);
-            }
-        }
-
-        private TaskCompletionSource<TResult>[] GetAndDeleteAll()
-        {
-            lock (_tasks)
-            {
-                var result = _tasks.Values.ToArray();
-                _tasks.Clear();
-                return result;
-            }
+            var result = _tasks.Values.ToArray();
+            _tasks.Clear();
+            return result;
 
         }
 
-        public void SetExceptionsToAll(Exception exception)
+        /// <summary>
+        /// Attempts to transition the task into the <see cref="F:System.Threading.Tasks.TaskStatus.Canceled"></see> state.
+        /// </summary>
+        /// <param name="key">A key of the task</param>
+        /// <param name="cancellationToken">Optional cancellation token</param>
+        public void Cancel([NotNull] object key, CancellationToken cancellationToken = default(CancellationToken))
         {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (_tasks.TryRemove(key, out var task))
+            {
+                task.TrySetCanceled(cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to transition all tasks into the <see cref="F:System.Threading.Tasks.TaskStatus.Canceled"></see> state.
+        /// </summary>
+        /// <param name="cancellationToken">An optional cancellation token</param>
+        public void CancelAll(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var keys = _tasks.Keys.ToArray();
+            foreach (var key in keys)
+            {
+                Cancel(key, cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to transition all tasks into the <see cref="F:System.Threading.Tasks.TaskStatus.Faulted"></see> state.
+        /// </summary>
+        /// <param name="exception">An <paramref name="exception"/> to set in to task</param>
+        public void SetExceptionsToAll([NotNull] Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
             var tasks = GetAndDeleteAll();
 
             foreach (var taskCompletionSource in tasks)
-                taskCompletionSource.SetException(exception);
-
+            {
+                taskCompletionSource.TrySetException(exception);
+            }
         }
-
     }
-
-
-
-
 }
---
+++ tests/Common.Tests/UtilsClass/TasksManagerTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Common.Tests.UtilsClass
+{
+    public class TasksManagerTest
+    {
+        private readonly TasksManager<int> _tasksManager;
+        public TasksManagerTest()
+        {
+            _tasksManager = new TasksManager<int>();
+        }
+
+        [Fact]
+        public void ShouldCreateNewTask()
+        {
+            var task = _tasksManager.Add(42);
+
+            Assert.NotNull(task);
+            Assert.Equal(TaskStatus.WaitingForActivation, task.Status);
+        }
+
+        [Fact]
+        public void ShouldCompleteTask()
+        {
+            var task = _tasksManager.Add(42);
+            _tasksManager.SetResult(42, 43);
+
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+        }
+
+
+        [Fact]
+        public void ShouldCancelTask()
+        {
+            var task = _tasksManager.Add(42);
+            _tasksManager.Cancel(42);
+            Assert.Equal(TaskStatus.Canceled, task.Status);
+        }
+
+
+        [Fact]
+        public void ShouldCancelAll()
+        {
+            var task1 = _tasksManager.Add(42);
+            var task2 = _tasksManager.Add(43);
+            _tasksManager.CancelAll();
+
+            Assert.Equal(TaskStatus.Canceled, task1.Status);
+            Assert.Equal(TaskStatus.Canceled, task2.Status);
+        }
+
+        [Fact]
+        public void ShouldCancelByTimeout()
+        {
+            var now = DateTime.Now;
+            using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.5)))
+            {
+                var task = _tasksManager.Add(42, cts.Token);
+
+                var cancelled = Task.WaitAny(task, Task.Delay(TimeSpan.FromSeconds(1)));
+
+                Assert.Equal(0, cancelled);
+                Assert.Equal(TaskStatus.Canceled, task.Status);
+                Assert.True((DateTime.Now - now).TotalSeconds < 1);
+                Assert.True((DateTime.Now - now).TotalSeconds >= 0.5);
+            }
+        }
+
+
+        [Fact]
+        public void ShouldSetException()
+        {
+            var task = _tasksManager.Add(42);
+            var exc = new InvalidOperationException();
+            _tasksManager.SetExceptionsToAll(exc);
+            Assert.Equal(TaskStatus.Faulted, task.Status);
+            Assert.Equal(exc, task.Exception.InnerExceptions[0]);
+        }
+    }
+}
