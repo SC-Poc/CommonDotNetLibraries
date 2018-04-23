3b5e6dcf70117b7fd6c091f7e9c52408dbc94aac
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2018-04-23 15:32:46 +0200

    Implement a multi-threading safe GetOrCreateAsync extension method for IMemoryCache.

---
+++ src/Lykke.Common/Cache/InMemoryExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Lykke.Common.Cache
+{
+    public static class InMemoryExtensions
+    {
+        private static readonly ConcurrentDictionary<IMemoryCache, object> Caches = new ConcurrentDictionary<IMemoryCache, object>();
+
+        /// <summary>
+        /// Returns an existed value or creates,caches and returns a new using the <paramref name="factory"/>
+        /// </summary>
+        /// <typeparam name="TItem">An cached item type</typeparam>
+        /// <param name="cache">An instance of <see cref="IMemoryCache"/></param>
+        /// <param name="key">A key</param>
+        /// <param name="factory">A factory method that creates a new value in case the key is absent in the cache</param>
+        /// <returns></returns>
+        public static Task<TItem> GetOrCreateAsync<TItem>([NotNull] this IMemoryCache cache, [NotNull] object key, [NotNull] Func<Task<TItem>> factory)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            if (cache.TryGetValue(key, out var obj))
+            {
+                return Task.FromResult((TItem)obj);
+            }
+
+            var missedCalls = (ConcurrentDictionary<object, Lazy<Task<TItem>>>)Caches.GetOrAdd(cache, _ => new ConcurrentDictionary<object, Lazy<Task<TItem>>>());
+            // It is a very little chance that factory() can be called twice.
+            // For this a thread must stop before calling missedCalls.GetOrAdd() and other thread in parallel must call  missedCalls.TryRemove(key, out _);
+            // But it practically impossible
+            var lazy = missedCalls.GetOrAdd(key, newKey =>
+                new Lazy<Task<TItem>>(() =>
+                {
+                    var factoryTask = factory();
+
+                    var t = factoryTask.ContinueWith(ancestor =>
+                    {
+
+                        var result = ancestor.GetAwaiter().GetResult(); // Use GetAwaiter() only for persisting a correct exception stack 
+                        var entry = cache.CreateEntry(newKey);
+                        entry.SetValue(result);
+                        entry.Dispose();
+                        return result;
+                    }, TaskContinuationOptions.ExecuteSynchronously);
+
+                    return t;
+                })
+            );
+
+            var promise = lazy.Value;
+            promise.ContinueWith(task => { missedCalls.TryRemove(key, out _); }, TaskContinuationOptions.ExecuteSynchronously);
+            return promise;
+
+
+        }
+    }
+}
---
+++ tests/Common.Tests/Cache/InMemoryCacheExtensionsTest.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Lykke.Common.Cache;
+using Xunit;
+
+namespace Common.Tests.Cache
+{
+    public class InMemoryCacheExtensionsTest
+    {
+        private readonly MemoryCache _cache;
+        private int _longTaskCalledNo;
+
+        public InMemoryCacheExtensionsTest()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions { ExpirationScanFrequency = TimeSpan.MaxValue });
+        }
+
+        [Fact]
+        public void GetOrCreateAsyncShouldHandleConcurrency()
+        {
+            var mock = new CacheMock(_cache);
+
+            var kv1 = new { key = "key1", value = 42 };
+            var kv2 = new { key = "key2", value = 43 };
+
+            var kvs = new[] { kv1, kv2 };
+            const int repeats = 100000;
+            var tasks = new ConcurrentBag<(int val, Task<int> task)>();
+            Parallel.For(0, repeats, (i, state) =>
+            {
+                var expected = kvs[i % 2];
+
+                tasks.Add((expected.value, mock.GetOrCreateAsync(expected.key, () => LongTask(expected.value))));
+            });
+
+            Task.WaitAll(tasks.Select(t => t.task).ToArray());
+
+            foreach (var task in tasks)
+            {
+                Assert.Equal(task.val, task.task.Result);
+            }
+
+            Assert.Equal(repeats, mock.TryGetValueCalledNo);
+            Assert.Equal(2, _longTaskCalledNo);
+
+
+            _cache.TryGetValue(kv1.key, out var cachedResult);
+            Assert.Equal(kv1.value, cachedResult);
+
+            _cache.TryGetValue(kv2.key, out var cachedResult2);
+            Assert.Equal(kv2.value, cachedResult2);
+
+            Assert.Equal(2, mock.CreateEntryCalledNo);
+        }
+
+        [Fact]
+        public async Task ShouldCorrectlyHandleCancellation()
+        {
+            var mock = new CacheMock(_cache);
+
+            const string key = "key";
+            var tasks = new List<Task<int>>();
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(mock.GetOrCreateAsync(key, LongTaskWithCancellation));
+            }
+
+            // ReSharper disable once CoVariantArrayConversion
+
+            try
+            {
+                await Task.WhenAll(tasks.ToArray());
+            }
+            catch (TaskCanceledException)
+            {
+                Assert.True(true);
+                return;
+            }
+
+            Assert.True(false);
+        }
+
+
+        [Fact]
+        public void FailedFactoryShouldNotAffectCache()
+        {
+            var exception = new InvalidOperationException("This is my exception");
+
+
+
+
+            var mock = new CacheMock(_cache);
+
+            const string key = "key";
+            var tasks = new List<Task<int>>();
+            for (var i = 0; i < 5; i++)
+            {
+                tasks.Add(mock.GetOrCreateAsync(key, () => LongTaskWithException(exception)));
+            }
+
+            try
+            {
+                // ReSharper disable once CoVariantArrayConversion
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException)
+            {
+            }
+            Assert.All(tasks, task =>
+            {
+                Assert.True(task.IsFaulted);
+                Assert.Equal(exception, task.Exception.InnerException);
+            });
+
+            Assert.Equal(1, _longTaskCalledNo);
+            Assert.Equal(0, mock.CreateEntryCalledNo);
+        }
+
+        private async Task<int> LongTaskWithException(Exception exception)
+        {
+            Interlocked.Increment(ref _longTaskCalledNo);
+            await Task.Delay(1000);
+            throw exception;
+        }
+
+        private async Task<int> LongTask(int result)
+        {
+            Interlocked.Increment(ref _longTaskCalledNo);
+            await Task.Delay(1000).ConfigureAwait(false);
+            return result;
+        }
+
+        private async Task<int> LongTaskWithCancellation()
+        {
+            Interlocked.Increment(ref _longTaskCalledNo);
+            using (var cst = new CancellationTokenSource(500))
+            {
+                await Task.Delay(1000, cst.Token).ConfigureAwait(false);
+            }
+            return 42;
+        }
+    }
+
+
+    internal class CacheMock : IMemoryCache
+    {
+        private readonly IMemoryCache _impl;
+        public int CreateEntryCalledNo;
+        public int TryGetValueCalledNo;
+
+        public CacheMock(IMemoryCache impl)
+        {
+            _impl = impl;
+        }
+
+        public void Dispose()
+        {
+            _impl.Dispose();
+        }
+
+        public bool TryGetValue(object key, out object value)
+        {
+            Interlocked.Increment(ref TryGetValueCalledNo);
+            return _impl.TryGetValue(key, out value);
+        }
+
+        public ICacheEntry CreateEntry(object key)
+        {
+            Interlocked.Increment(ref CreateEntryCalledNo);
+            return _impl.CreateEntry(key);
+        }
+
+        public void Remove(object key)
+        {
+            _impl.Remove(key);
+        }
+    }
+}
