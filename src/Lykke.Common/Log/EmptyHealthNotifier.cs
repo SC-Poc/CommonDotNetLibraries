a750d2cfa8be5cf9de3026ab64dce3d79bf10fb8
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-05-24 15:18:03 +0300

    Logging system redesign. Empty health notifier has been added

---
+++ src/Lykke.Common/Log/EmptyHealthNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Lykke.Common.Log
+{
+    /// <summary>
+    /// Health notifier, that notifies nobody. Could be used in tests
+    /// </summary>
+    [PublicAPI]
+    public class EmptyHealthNotifier : IHealthNotifier
+    {
+        public Task NotifyAsync(string healthMessage, object context = null)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
