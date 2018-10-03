8a90c729b201f4f0736c62c46b9e0034967b5f2d
some-dev <avolkov.dev@gmail.com>
2018-10-03 22:28:16 +0300

    LWDEV-9750 add in memory password initialization service with data protection (based on sign facade used MasterPaswordService - just moved common parts to Lykke.Common nuget package)

---
+++ src/Lykke.Common/MasterPassword/IPasswordProtectorService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Common.MasterPassword
+{
+    public interface IPasswordProtectorService
+    {
+        Task SetPasswordKeyAsync(int partNumber, string partValue);
+
+        Task ValidatePasswordAsync(byte[] validationBytes);
+
+        bool IsPasswordValid { get; }
+    }
+}
---
+++ src/Lykke.Common/MasterPassword/IPasswordValidationHashRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lykke.Common.MasterPassword
+{
+    public interface IPasswordValidationHashRepository
+    {
+        Task ClearAsync();
+
+        Task<IEnumerable<string>> GetAsync();
+
+        Task InsertAsync(IEnumerable<string> validationHashes);
+    }
+}
