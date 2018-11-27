ffd4503715752c8c6ac79b2ed1182e5bc0e2bdef
tarurar <tarutin_andrew@mail.ru>
2018-11-27 14:01:42 +0300

    fixed code review comments

--- src/Lykke.Common/CountryItem.cs
+++ src/Lykke.Common/CountryItem.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿using System;
+using Common;
 
 namespace Lykke.Common
 {
@@ -11,6 +12,12 @@ namespace Lykke.Common
 
         public CountryItem(string iso3, string prefix)
         {
+            if (!CountryManager.HasIso3(iso3))
+                throw new ArgumentException($"ISO3 code doesn't exist ({iso3})", nameof(iso3));
+
+            if (string.IsNullOrWhiteSpace(prefix))
+                throw new ArgumentNullException(nameof(prefix));
+
             Id = iso3;
             Iso2 = CountryManager.Iso3ToIso2(iso3);
             Name = CountryManager.GetCountryNameByIso3(iso3);
--- tests/Common.Tests/CountryPhoneCodesTests.cs
+++ tests/Common.Tests/CountryPhoneCodesTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Lykke.Common;
 using Xunit;
 
@@ -18,6 +19,27 @@ namespace Common.Tests
             //Assert
             Assert.All(countries, x => Assert.False(string.IsNullOrEmpty(x.Iso2)));
             Assert.All(countries, x => Assert.False(string.IsNullOrEmpty(x.Name)));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        [InlineData("whatever")]
+        public void NewCountryItem_InvalidIso3Code_RaisesException(string iso3)
+        {
+            Assert.Throws<ArgumentException>(() => new CountryItem(iso3, "any phone prefix"));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void NewCountryItem_InvalidPrefix_RaisesException(string prefix)
+        {
+            var validIso3 = CountryManager.CountryIso3ToNameLinks.First().Key;
+
+            Assert.Throws<ArgumentNullException>(() => new CountryItem(validIso3, prefix));
         }
     }
 }
